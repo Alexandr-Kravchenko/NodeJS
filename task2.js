729c72ea8fef55c491db25b0c2e14d00a7eb86ab
Alexandr-Kravchenko <73229640+Alexandr-Kravchenko@users.noreply.github.com>
2021-07-19 18:51:09 +0300

    JS Intro

---
+++ task2.js
@@ -0,0 +1,12 @@
+let sumOfRange = function(number1, number2) {
+    let range = [];
+    for (let i = number1; i <= number2; i += 1 ){
+        range.push(i)
+    }
+    let result = range.reduce((prevNumber, currentNumber) => {
+        return prevNumber + currentNumber
+    });
+    return result
+}
+
+console.log(sumOfRange(5, 7));
