47260d684db5f81b109bbc960bd67c93023b171a
Oleksandr Kravchenko <intern@intern-pc.interlink.office>
2021-07-20 18:58:12 +0300

    pluralisation is work, but with problem

--- task1.js
+++ task1.js
@@ -1,3 +1,14 @@
-let pl = function(number, one, few, many) {
+let pl = function(num, one, few, many) {
+	num = num % 100;
+	let lastPos = num % 10;
+	let result;
+	if(num > 10 && num < 20) result = many;
+	if(lastPos === 0) result = many;
+	if(lastPos >= 5 && lastPos <= 9) result = many;
+	if(lastPos === 1) result = one;
+	if(lastPos >= 2 && lastPos <= 4) result = few;
 
+	return result
 }
+
+console.log(pl(511, 'кнопка', 'кнопки', 'кнопок'));
